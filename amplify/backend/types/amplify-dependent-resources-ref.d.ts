0e4398302ced2303110d9cf58a2ffa5b57747de9
Kisan Majumdar <kisanmajumdar44@gmail.com>
2025-04-09 22:31:19 +0530

    so complex you are amplify but easy if know how to setup

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,13 @@
+export type AmplifyDependentResourcesAttributes = {
+  "auth": {
+    "awschat939d5c4e": {
+      "AppClientID": "string",
+      "AppClientIDWeb": "string",
+      "IdentityPoolId": "string",
+      "IdentityPoolName": "string",
+      "UserPoolArn": "string",
+      "UserPoolId": "string",
+      "UserPoolName": "string"
+    }
+  }
+}
